45b1544e26369a2ccb246f58199e826094b33a02
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-09 22:39:28 +0530

    Allow server to run without database

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const db = mysql.createConnection({
 db.connect((err) => {
   if (err) {
     console.error("❌ Database connection failed:", err.message);
-    process.exit(1);
+
   }
   console.log("✅ Connected to MySQL Database");
 });
