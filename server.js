958a57610f4fcc533d87a8e75edc285c0ee28f59
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-13 20:13:26 +0530

    Update MySQL connection using DATABASE_URL

--- server.js
+++ server.js
@@ -23,16 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
