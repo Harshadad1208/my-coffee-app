7161ae4705d6b0bc1424e55be8edbac0f026ae54
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-13 20:29:37 +0530

    My_SQL connection

--- server.js
+++ server.js
@@ -23,15 +23,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ CRITICAL: MySQL Connection Failed!");
-    console.error("Error Details:", err.message);
-  } else {
-    console.log("✅ SUCCESS: Connected to MySQL Database");
-    initDB();
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
