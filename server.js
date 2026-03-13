bced1ccb13a6f97e2366a956f2c2fba17b18e1d8
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-13 19:15:45 +0530

    Fix DB_PASSWORD variable

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -25,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
