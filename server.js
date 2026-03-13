95051e2873b81119a122a8d793ac7c12fadcb904
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-13 19:52:45 +0530

    Add SSL support for Aiven MySQL

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
@@ -37,6 +40,7 @@ db.connect((err) => {
     console.error("Error Details:", err.message);
   } else {
     console.log("✅ SUCCESS: Connected to MySQL Database");
+    initDB();
   }
 });
 
