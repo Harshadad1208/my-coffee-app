e46015e6866637cfefd3a156d1dec5b36b1a9c2f
Harshada deshmukh <Harshadad197@gmail.com>
2026-03-14 11:27:25 +0530

    Connected Railway MySQL database

--- server.js
+++ server.js
@@ -25,16 +25,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionLimit: 10
 });
 
 
